1ec6d17b90858d413f277028bdf8c635ac50dae4
illiaavsitidiiyskyi <_email@example.com>
2026-07-28 07:49:31 +0200

    Add multi-tab support

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,65 @@
 import { app, BrowserWindow, BrowserView, ipcMain } from 'electron';
 
 let mainWindow: BrowserWindow;
-let view: BrowserView;
+let views: BrowserView[] = [];
+let activeViewIndex = 0;
+
+const TOOLBAR_HEIGHT = 80;
+
+function createTab(url: string = 'https://www.google.com') {
+  const view = new BrowserView();
+  views.push(view);
+  activeViewIndex = views.length - 1;
+
+  view.webContents.loadURL(url);
+  mainWindow.setBrowserView(view);
+  resizeActiveView();
+
+  view.webContents.on('did-navigate', () => {
+    sendTabsUpdate();
+  });
+
+  sendTabsUpdate();
+}
+
+function resizeActiveView() {
+  const bounds = mainWindow.getBounds();
+  views[activeViewIndex].setBounds({
+    x: 0,
+    y: TOOLBAR_HEIGHT,
+    width: bounds.width,
+    height: bounds.height - TOOLBAR_HEIGHT
+  });
+}
+
+function switchTab(index: number) {
+  activeViewIndex = index;
+  mainWindow.setBrowserView(views[index]);
+  resizeActiveView();
+  sendTabsUpdate();
+}
+
+function closeTab(index: number) {
+  views[index].webContents.close();
+  views.splice(index, 1);
+
+  if (views.length === 0) {
+    createTab();
+    return;
+  }
+
+  activeViewIndex = Math.min(index, views.length - 1);
+  switchTab(activeViewIndex);
+}
+
+function sendTabsUpdate() {
+  const tabs = views.map((v, i) => ({
+    index: i,
+    url: v.webContents.getURL(),
+    active: i === activeViewIndex
+  }));
+  mainWindow.webContents.send('tabs-updated', tabs);
+}
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -14,18 +72,26 @@ function createWindow() {
 
   mainWindow.loadFile('src/renderer/index.html');
 
-  view = new BrowserView();
-  mainWindow.setBrowserView(view);
-  view.setBounds({ x: 0, y: 80, width: 1200, height: 720 });
-  view.webContents.loadURL('https://www.google.com');
+  createTab();
 
   mainWindow.on('resize', () => {
-    const bounds = mainWindow.getBounds();
-    view.setBounds({ x: 0, y: 80, width: bounds.width, height: bounds.height - 80 });
+    resizeActiveView();
   });
 
   ipcMain.on('navigate', (_event, url: string) => {
-    view.webContents.loadURL(url);
+    views[activeViewIndex].webContents.loadURL(url);
+  });
+
+  ipcMain.on('new-tab', () => {
+    createTab();
+  });
+
+  ipcMain.on('switch-tab', (_event, index: number) => {
+    switchTab(index);
+  });
+
+  ipcMain.on('close-tab', (_event, index: number) => {
+    closeTab(index);
   });
 }
 
--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,5 +1,11 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('browserAPI', {
-  navigate: (url: string) => ipcRenderer.send('navigate', url)
+  navigate: (url: string) => ipcRenderer.send('navigate', url),
+  newTab: () => ipcRenderer.send('new-tab'),
+  switchTab: (index: number) => ipcRenderer.send('switch-tab', index),
+  closeTab: (index: number) => ipcRenderer.send('close-tab', index),
+  onTabsUpdated: (callback: (tabs: any[]) => void) => {
+    ipcRenderer.on('tabs-updated', (_event, tabs) => callback(tabs));
+  }
 });
